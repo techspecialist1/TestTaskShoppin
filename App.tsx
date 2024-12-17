7405fe8534614435ecb600d4e6c33423ca13b3e5
techspecialist1 <techspecialist840@gmail.com>
2024-12-17 15:20:40 +0530

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>This is Initial Screen</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App
